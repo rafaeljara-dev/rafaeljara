61377a8045ae3cbce0d874bb27bcceec4e8dcb58
jara77 <rafaeljara73@gmail.com>
2024-05-07 23:58:52 -0600

    Images of proyectos first view

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -1,29 +1,48 @@
 "use client"
 
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle
+} from "./ui/card"
+
+import Image from "next/image";
+
+import sait from "@/assets/images/(laptop-images)/sait.png"
+import itsonmeet from "@/assets/images/(laptop-images)/itsonmeet.png"
+import lnm from "@/assets/images/(laptop-images)/liga-norte.png"
+import algodoneros from "@/assets/images/(laptop-images)/algodoneros.png"
 
 const testimonials = [
     {
-        name: "Armando Fernandez",
+        name: "It´s On Meet",
         avatar: "A",
-        title: "Software Engineer",
-        description: "This is the best application i´ve use"
-
+        title: "Mobile App, Website",
+        description: "Social Media app to connect with experts",
+        image: itsonmeet,
     },
     {
-        name: "Armando Fernandez",
+        name: "Sait Software",
         avatar: "A",
-        title: "Software Engineer",
-        description: "This is the best application i´ve use"
-
+        title: "Website, Web App",
+        description: "Website for a administrative software and aplications",
+        image: sait,
     },
     {
-        name: "Armando Fernandez",
+        name: "Liga Norte de México",
         avatar: "A",
-        title: "Software Engineer",
-        description: "This is the best application i´ve use"
-
-    }
+        title: "Website, Blog",
+        description: "Diseño para liga de beisbol AA del norte de México",
+        image: lnm,
+    },
+    {
+        name: "Algodoneros de San Luis",
+        avatar: "A",
+        title: "Website, Blog",
+        description: "Equipo nacional de beisbol de la LNM",
+        image: algodoneros,
+    },
 ]
 
 export const LandingContent = () => {
@@ -35,6 +54,14 @@ export const LandingContent = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {testimonials.map((item) => (
                     <Card key={item.description} className="bg-[#192339] border-none text-white">
+                        <div className="flex items-center justify-center m-2">
+                            <Image
+                                src={item.image}
+                                width={250}
+                                height={100}
+                                alt="Picture of the author"
+                            />
+                        </div>
                         <CardHeader>
                             <CardTitle className="flex items-center gap-x-2">
                                 <div>

428f8a70e12c14907368063f5933cd1dcfabce2a
jara77 <rafaeljara73@gmail.com>
2024-05-21 23:21:03 -0600

    Header Content Degreed

--- app/(landing)/(links)/(pre-proyectos)/pre-proyectos/page.tsx
+++ app/(landing)/(links)/(pre-proyectos)/pre-proyectos/page.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import HeaderContent from "@/components/header-content";
 import LandingNavbar from "@/components/landing-navbar";
-import algodoneros from "@/assets/images/(laptop-images)/algodoneros.png";
 
 const PreProyectos: React.FC = () => {
-    const headerImage = algodoneros; // Usamos directamente la importación de la imagen
+    const gradientFrom = "#00FF00"; // Color inicial del degradado (verde)
+    const gradientTo = "#000000";   // Color final del degradado (negro)
     const headerText = "Pre Proyectos";
 
     return (
         <div>
             <LandingNavbar />
-            <HeaderContent imageSrc={headerImage} text={headerText} />
+            <HeaderContent gradientFrom={gradientFrom} gradientTo={gradientTo} text={headerText} />
         </div>
     );
 }
--- components/header-content.tsx
+++ components/header-content.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import Image from 'next/image';
 
 interface HeaderContentProps {
-    imageSrc: StaticImageData; // Tipo específico para las imágenes estáticas importadas
+    gradientFrom: string; // Color inicial del degradado
+    gradientTo: string;   // Color final del degradado
     text: string;
 }
 
-const HeaderContent: React.FC<HeaderContentProps> = ({ imageSrc, text }) => {
+const HeaderContent: React.FC<HeaderContentProps> = ({ gradientFrom, gradientTo, text }) => {
     return (
-        <div className="header-content flex flex-col items-center justify-center bg-black p-4">
-            <Image src={imageSrc} alt="Header Image" className="w-full flex" />
+        <div
+            className="header-content flex flex-col items-center justify-center p-4"
+            style={{ 
+                background: `linear-gradient(to right, ${gradientFrom}, ${gradientTo})` 
+            }}
+        >
             <h1 className="text-white text-center mt-4">{text}</h1>
         </div>
     );
